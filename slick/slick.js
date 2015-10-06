04aa7515811337b81b9b8706ec17a5b1c35363d7
Andrei Balmus <abalmus@tacitknowledge.com>
2015-10-06 14:48:15 +0300

    other way of CJS and AMD

--- slick/slick.js
+++ slick/slick.js
@@ -18,9 +18,11 @@
 (function(factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
+        define(function () {
+            return factory;
+        });
     } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
+        module.exports = factory;
     } else {
         factory(jQuery);
     }
